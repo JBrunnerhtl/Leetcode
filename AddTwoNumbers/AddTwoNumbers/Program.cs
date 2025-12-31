18f13e03882f553563a2c2759f8e87a9bb1ec5d2
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2025-12-31 15:52:38 +0100

    next task started. some errors left

---
+++ AddTwoNumbers/AddTwoNumbers/Program.cs
@@ -0,0 +1,69 @@
+﻿public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+     }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3)));
+        ListNode l2 = new ListNode(4, new ListNode(5));
+        Solution sln = new Solution();
+        var output = sln.AddTwoNumbers(l1, l2);
+        Console.WriteLine(output); 
+    }
+}
+
+
+
+
+ 
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        string list1 = "";
+        string list2 = "";
+        ListNode head1 = l1;
+        ListNode head2 = l2;
+        while (head1 != null || head2 != null)
+        {
+            if (head1 != null)
+            {
+                list1 += head1.val.ToString();
+                head1 = head1.next;    
+            }
+
+            if (head2 != null)
+            {
+                list2 += head2.val.ToString();
+                head2 = head2.next;
+            }
+        }
+        string reversed1 = new string(list1.Reverse().ToArray());
+        string reversed2 = new string(list2.Reverse().ToArray());
+        int sum =int.Parse(reversed1) +  int.Parse(reversed2);
+        
+        char[] parts = sum.ToString().Reverse().ToArray();
+        if (parts.Length > 0)
+        {
+            
+            ListNode head = new ListNode(int.Parse(parts[0].ToString()));
+            ListNode previous = head;
+            for (int i = 1; i < parts.Length; i++)
+            {
+                ListNode newOne = new ListNode(int.Parse(parts[i].ToString()));
+                previous.next = newOne;
+                previous = newOne;
+            }
+            return head;
+        }
+
+        return new ListNode();
+
+    }
+}
