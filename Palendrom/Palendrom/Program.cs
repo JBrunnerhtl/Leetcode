c50ca83513c77f6908ad3a3b62c5630f89474097
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2026-01-02 09:57:31 +0100

    finished third assigment. Function IsPalindrome has a runtime of 2 ms and my code beats 74.60% of all other

---
+++ Palendrom/Palendrom/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Palendrom;
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+public class Solution {
+    public bool IsPalindrome(int x)
+    {
+        string tmp = x.ToString();
+        for (int i = 0, j = tmp.Length-1; i < tmp.Length; i++, j--)
+        {
+            if (tmp[i] != tmp[j])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
